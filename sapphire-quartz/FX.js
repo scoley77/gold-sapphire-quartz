2c1926d1c5a200feeb9548448d591a061d1db52e
Sara Coley <79733957+scoley77@users.noreply.github.com>
2022-02-12 17:13:40 -0700

    Add files via upload

---
+++ sapphire-quartz/FX.js
@@ -0,0 +1,12 @@
+const splash = document.querySelector('.loading-container');
+
+const fade = function() {
+  setTimeout(()=>{
+    document.querySelector('#loading-container').style.opacity = '0';
+  }, 2000);
+  setTimeout(()=>{
+    document.querySelector('.invite-container').style.opacity = '1';
+  }, 3000);
+};
+
+fade();
